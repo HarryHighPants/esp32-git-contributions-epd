517d14a96527a82e93768c42b94230b15b11002d
Dave Allie <dave@daveallie.com>
2025-05-23 01:53:38 +1000

    Modularise code and convert to PlatformIO project

---
+++ src/DeviceConfig.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <GxEPD2_4G_4G.h>
+
+#define WEEKS 17
+#define BUTTON_PIN 39
+#define BAT_TEST_PIN 35
+// 4 color greyscale
+#define GxEPD2_DISPLAY_CLASS GxEPD2_4G_4G
+// GDEY0213B74 122x250, SSD1680, (FPC-A002 20.04.08)
+#define GxEPD2_DRIVER_CLASS GxEPD2_213_GDEY0213B74
+
+// TODO: Add additional device types
+// // 4 color greyscale
+// #define GxEPD2_DISPLAY_CLASS GxEPD2_4G_4G
+// // DEPG0266BN 152x296, SSD1680, (FPC-A002 20.04.08)
+// #define GxEPD2_DRIVER_CLASS GxEPD2_266_DEPG0266BN
