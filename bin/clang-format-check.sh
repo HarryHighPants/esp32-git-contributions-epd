a8f22ef46f9c05b0fdbb8d87f713e44ff0dcc6dd
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2025-05-24 13:52:52 +1000

    Add clang format config

--- bin/clang-format-check.sh
+++ bin/clang-format-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find src \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -style=google -i -n --Werror {} +
+find ../src \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -style=google -i -n --Werror {} +
 
 if [[ $? -ne 0 ]]; then
   echo "Code style issues found. Please run './bin/clang-format-fix.sh' to fix them."
