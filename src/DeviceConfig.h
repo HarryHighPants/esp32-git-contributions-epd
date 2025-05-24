8ef2961643c3d4a795d620c6e4fb81eb171de0a7
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2025-05-24 13:23:00 +1000

    Add lilygo213 config as default

--- src/DeviceConfig.h
+++ src/DeviceConfig.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <GxEPD2_4G_4G.h>
 
+// LILYGO213
 #define WEEKS 17
 #define BUTTON_PIN 39
 #define BAT_TEST_PIN 35
@@ -8,9 +9,7 @@
 #define GxEPD2_DISPLAY_CLASS GxEPD2_4G_4G
 // GDEY0213B74 122x250, SSD1680, (FPC-A002 20.04.08)
 #define GxEPD2_DRIVER_CLASS GxEPD2_213_GDEY0213B74
-
-// TODO: Add additional device types
-// // 4 color greyscale
-// #define GxEPD2_DISPLAY_CLASS GxEPD2_4G_4G
-// // DEPG0266BN 152x296, SSD1680, (FPC-A002 20.04.08)
-// #define GxEPD2_DRIVER_CLASS GxEPD2_266_DEPG0266BN
+#define EPD_PIN_CS 5
+#define EPD_PIN_DC 17
+#define EPD_PIN_RST 16
+#define EPD_PIN_BUSY 4
